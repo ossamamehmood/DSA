b8bb5afb07d5b44feaa9cb3eb5fc728b861a781f
Ossama Mehmood <ossamamehmood110@gmail.com>
2021-10-21 15:21:58 +0500

    Double Linklist

--- Linkelists/Double Linklist/DoubleLinklist/src/doublelinklist/DoubleLinklist.java
+++ Linkelists/Double Linklist/DoubleLinklist/src/doublelinklist/DoubleLinklist.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package circularlinklist;
+package doublelinklist;
 import java.util.Scanner;
 
 /**
@@ -12,132 +12,166 @@ import java.util.Scanner;
  */
 
 class node {
-	int info;
-	node link;
 	
+	int info;
+
+	node next;
+
+	node prev;
+
 }
 
-public class CircularLinklist {
+
+public class DoubleLinklist {
 
     /**
      * @param args the command line arguments
      */
     
-	static node last;
-	
-	static void create_list(int data) {
-		if (last == null) {
-			last = new node();
-			last.info = data;
-			last.link = last;
-		} else {
+	static node start;
+
+
+	public static void create_list(int data) {
+		
+	if (start == null) {
+			
+	start = new node();
+			
+	start.info = data;
+		}
+ else {
+			
+	node q = start;
+			while (q.next != null)
+				q = q.next;
 			node temp = new node();
 			temp.info = data;
-			temp.link = last.link;
-			last.link = temp;
-			last = temp;
+			q.next = temp;
+			temp.prev = q;
+
 		}
 	}
 
 	static void addatbeg(int data) {
-		if (last == null) {
-			last = new node();
-			last.info = data;
-			last.link = last;
-		} else {
-			node temp = new node();
-			temp.info = data;
-			temp.link = last.link;
-			last.link = temp;
-		}
-
+		node tmp = new node();
+		tmp.info = data;
+		tmp.next = start;
+		tmp.prev = null;
+ start.prev = tmp;		start = tmp;
 	}
 
 	static void addafter(int data, int pos) {
-		node temp = last.link;
-		for(int i=0; i<pos-1;i++) {
-			temp = temp.link;
-			if(temp == last && i != pos-2) {
-				System.out.println("Index out of bound");
+		node tmp, q;
+		int i;
+		q = start;
+		for (i = 0; i < pos; i++) {
+			q = q.next;
+			if (q == null) {
+				System.out.println("There are less than " + pos + " elements");
 				return;
 			}
 		}
-		if(temp == last)
-			create_list(data);
-		else {
-			node n = new node();
-			n.info = data;
-			n.link = temp.link;
-			temp.link = n;
-		}
+		tmp = new node();
+		tmp.next = q.next;
+		tmp.info = data;
+		q.next = tmp;
+		tmp.prev = q;
 	}
 
-	static void del(int data) {
-		if(last.link == last && last.info == data) {
-			last = null;
-		}else {
-			node q = last;
-			while(q.link != last) {
-				if(q.link.info == data) {
-					q.link = q.link.link;
-					return;
-				}
-				q = q.link;
-			}
-			if(q.link.info == data) {
-				q.link = q.link.link;
-				last = q;
+	public static void del(int data) {
+		node tmp, q;
+		if (start.info == data) {
+			tmp = start;
+			start = start.next; /* first element deleted */
+			if(start != null)
+				start.prev = null;
+			// delete(tmp);
+			return;
+		}
+		q = start;
+		while (q.next.next != null) {
+			if (q.next.info == data) /* element deleted in between */
+			{
+				tmp = q.next;
+				q.next = tmp.next;
+				tmp.next.prev = q;
+				// free(tmp);
 				return;
 			}
+			q = q.next;
+		}
+		if (q.next.info == data) /* last element deleted */
+		{
+			tmp = q.next;
+
+			q.next = null;
+			return;
 		}
 		System.out.println("Element " + data + " not found");
 	}
 
-	static void display() {
-		if (last == null) {
+	public static void display() {
+		node q;
+		if (start == null) {
 			System.out.println("List is empty");
 			return;
 		}
-		node q = last.link;
-		System.out.print("List is: ");
-		while (q != last) {
-			System.out.print(q.info + "   ");
-			q = q.link;
+		q = start;
+		System.out.println("List is:");
+		while (q != null) {
+			System.out.println(q.info + "   ");
+			q = q.next;
 		}
-		System.out.println(q.info);
+		System.out.println();
 	}
 
 	static void count() {
-		if (last == null) {
-			System.out.println("List is empty");
-			return;
-		}
-		node q = last.link;
-		int cnt =1;
-		while (q != last) {
+		node q = start;
+		int cnt = 0;
+		while (q != null) {
+			q = q.next;
 			cnt++;
-			q = q.link;
 		}
 		System.out.println("Number of elements are " + cnt);
 	}
 
-	static void search(int data) {
-		node ptr = last.link;
+	public static void rev() {
+		node p1, p2;
+		if (start.next == null)
+			return; /* only one element */
+
+		p1 = start;
+		p2 = p1.next;
+
+		p1.next = null;
+		p1.prev = p2;
+		while (p2 != null) {
+			p2.prev = p2.next;
+			p2.next = p1;
+			p1 = p2;
+			p2 = p2.prev; /* next of p2 changed to prev */
+		}
+		start = p1;
+	}
+
+	public static void search(int data) {
+		node ptr = start;
 		int pos = 1;
-		do {
+		while (ptr != null) {
 			if (ptr.info == data) {
 				System.out.println("Item " + data + " found at position " + pos);
 				return;
 			}
-			ptr = ptr.link;
+			ptr = ptr.next;
 			pos++;
-		}while (ptr != last);
-		System.out.println("Item " + data + " not found in list");
+		}
+		if (ptr == null)
+			System.out.println("Item " + data + " not found in list");
 	}
 
 	public static void main(String[] args) {
         // TODO code application logic here
-        
+
 		Scanner cin = new Scanner(System.in);
 
 		int choice, n, m, position, i;
@@ -171,36 +205,28 @@ public class CircularLinklist {
 			System.out.print("\nEnter yours choice : ");
 			choice = cin.nextInt();
 			switch (choice) {
-			
-                            case 1:
-                                
+			case 1:
 				System.out.print("\nEnter the element : ");
 				n = cin.nextInt();
 				for (i = 0; i < n; i++) {
-					System.out.print("\nEnter yours choice : ");
+					System.out.println("\nEnter the element");
 					m = cin.nextInt();
 					create_list(m);
 				}
 				break;
-			
-                            case 2:
-                                
+			case 2:
 				System.out.println("\nEnter the element");
 				m = cin.nextInt();
 				addatbeg(m);
 				break;
-			
-                            case 3:
-                            
+			case 3:
 				System.out.println("\nEnter the element");
 				m = cin.nextInt();
 				System.out.println("\nEnter the position after which this element is inserted");
 				position = cin.nextInt();
 				addafter(m, position);
 				break;
-			
-                            case 4:
-                                
+			case 4:
 				if (start == null) {
 					System.out.println("\nList is empty");
 					continue;
@@ -209,30 +235,24 @@ public class CircularLinklist {
 				m = cin.nextInt();
 				del(m);
 				break;
-			
-                            case 5:
-                                
+			case 5:
 				display();
 				break;
-			
-                            case 6:
-                                
+			case 6:
 				count();
 				break;
-
-                            case 7:
-                                
+			case 7:
+				rev();
+				break;
+			case 8:
 				System.out.println("\nEnter the element to be searched");
 				m = cin.nextInt();
 				search(m);
 				break;
-			
-                            case 8:
-                                
+			case 9:
 				rep = false;
 				break;
-			
-                            default:
+			default:
 				System.out.println("\nWrong choice");
 			}
 		}
